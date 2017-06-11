ec2bfd86ee73db656f7beab5cc173f16c9a8958e
ericgiannini <eric.giannini@yahoo.com>
2017-06-11 14:02:55 -0700

    Added examples

--- Node.playground/Contents.swift
+++ Node.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 var str = "Hello, playground"
 
-public class Node<T:Comparable> {
+public class Node< T : Comparable > {
     //Value and children vars
     public var value:T
     public var childLeft:Node?
@@ -12,17 +12,19 @@ public class Node<T:Comparable> {
     public weak var parent:Node?
     
     //Initialization
-    public convenience init(value: T) {
-        self.init(value: value)
-    }
-    
-    public init(value:T, left:Node , right:Node ,
-                parent:Node ) {
+    public init(value:T, left:Node?, right:Node?,
+        parent:Node?) {
         self.value = value
         self.childLeft = left
         self.childRight = right
         self.parent = parent
     }
+    
+    //Initialization
+    public convenience init(value: T) {
+        self.init(value: value, left: nil, right: nil, parent:nil)
+    }
+    
     
     // methods
     
@@ -37,11 +39,11 @@ public class Node<T:Comparable> {
     
     private func addNode(value:T) {
         
-        /* 
+        /*
          
          The value is less than root value; insert it in the left subtree. Insert it into the left subtree if it exists; if not, create a new node and insert it as the left child
          
-        */
+         */
         
         if value < self.value {
             if let childLeft = childLeft {
@@ -51,11 +53,11 @@ public class Node<T:Comparable> {
                 newNode.parent = self
                 childLeft = newNode
             }
-        /*
-
-         The value is greater than root value; insert it in the right subtree. Insert it into the right subtree if it exists; if not, create a new node and insert it as the right child
-         
-        */
+            /*
+             
+             The value is greater than root value; insert it in the right subtree. Insert it into the right subtree if it exists; if not, create a new node and insert it as the right child
+             
+             */
             
         } else {
             if let childRight = childRight {
@@ -70,5 +72,16 @@ public class Node<T:Comparable> {
         
     }
 }
+
+let rootNode = Node(value: 10)
+
+rootNode.insertNodeFromRoot(value: 50)
+rootNode.insertNodeFromRoot(value: 75)
+
+rootNode.insertNodeFromRoot(value: 100)
+rootNode.insertNodeFromRoot(value: 25)
+
+
+
 
 
